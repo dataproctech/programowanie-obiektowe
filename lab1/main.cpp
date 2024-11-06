ed7c0477556189f6e86d022174fcb1c11bfeb9ef
Serhii Baraban <akakieva@gmail.com>
2024-11-06 17:55:44 +0100

    Created main.cpp

---
+++ lab1/main.cpp
@@ -0,0 +1,18 @@
+// include the necessary libraries
+
+class Car {
+    //attributes
+
+    // Constructor
+
+    // Method to display car information
+   
+};
+
+int main() {
+    // Create a Car object
+    
+    // Display car details
+    
+    return 0;
+}

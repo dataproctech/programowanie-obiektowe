88c6241c1d4b373ab92136ac0f77f8495384dce9
Serhii Baraban <akakieva@gmail.com>
2024-11-06 17:55:07 +0100

    Created Car.java

---
+++ lab1/Car.java
@@ -0,0 +1,16 @@
+public class Car {
+    // Attributes
+    
+    // Constructor
+    
+    // Method to display car information
+    
+    }
+
+    public static void main(String[] args) {
+        // Create a Car object
+        
+        // Display car details
+        
+    }
+}

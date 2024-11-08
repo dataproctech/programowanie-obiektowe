7801cd0a0baa0c46c1b0010654ca2898dfeea958
Serhii Baraban <akakieva@gmail.com>
2024-11-08 10:16:48 +0100

    Created pracownik.cpp

---
+++ lab4/pracownik.cpp
@@ -0,0 +1,23 @@
+// #include 
+
+class Pracownik {
+protected:
+   
+public:
+   
+    // Metoda wyświetlająca stanowisko (dostępna tylko dla klas dziedziczących)
+    
+};
+
+class Nauczyciel {
+
+};
+
+class Administracja {
+
+};
+
+int main() {
+   
+    return 0;
+}

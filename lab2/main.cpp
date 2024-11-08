4f1e85cab79d6b21102c435d5b7f64b7e6dfaac9
Serhii Baraban <akakieva@gmail.com>
2024-11-08 10:13:33 +0100

    Created main.cpp

---
+++ lab2/main.cpp
@@ -0,0 +1,23 @@
+// #include
+
+class Samochod {
+private:
+
+public:
+    // Konstruktor domyślny
+   
+    // Konstruktor z argumentami
+    
+    // Destruktor
+   
+    // Metoda wyświetlania danych
+    
+};
+
+int main() {
+    // Utworzenie obiektu z użyciem konstruktora domyślnego
+
+    // Utworzenie obiektu z użyciem konstruktora z argumentami
+
+    return 0;  // Destruktory są wywoływane tutaj
+}

5092d9b61e5f07fc39b8f94af72d3adccb3afe34
Serhii Baraban <akakieva@gmail.com>
2024-11-08 10:14:31 +0100

    Created tablica.cpp

---
+++ lab2/tablica.cpp
@@ -0,0 +1,21 @@
+// #include 
+
+class Tablica {
+
+    // Konstruktor domyślny
+    
+    // Konstruktor z parametrem
+    
+    // Destruktor
+   
+};
+
+int main() {
+    // Tworzenie obiektów Tablica
+  
+    // Użycie konstruktora domyślnego
+  
+    // Użycie konstruktora z argumentem
+
+    return 0;
+}

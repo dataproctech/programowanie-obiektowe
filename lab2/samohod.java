c0bd019ab553a4af1ada01f3a5a9c59b82d777f0
Serhii Baraban <akakieva@gmail.com>
2024-11-08 10:14:04 +0100

    Created samohod.java

---
+++ lab2/samohod.java
@@ -0,0 +1,14 @@
+public class Samochod {
+  
+    // Konstruktor domyślny
+    
+    // Konstruktor z argumentami
+   
+    // Metoda wyświetlająca dane
+   
+    public static void main(String[] args) {
+        // Utworzenie obiektu z użyciem konstruktora domyślnego
+       
+        // Utworzenie obiektu z użyciem konstruktora z argumentami
+    }
+}

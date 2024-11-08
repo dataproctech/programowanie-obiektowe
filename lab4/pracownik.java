2c53e6c0822854edf2e46f5f290c6f02e9ffe4e9
Serhii Baraban <akakieva@gmail.com>
2024-11-08 10:17:09 +0100

    Created pracownik.java

---
+++ lab4/pracownik.java
@@ -0,0 +1,18 @@
+class Pracownik {
+   
+    // Konstruktor
+   
+
+class Nauczyciel  {
+   
+}
+
+class Administracja  {
+    
+}
+
+public class Main {
+    public static void main(String[] args) {
+        
+    }
+}

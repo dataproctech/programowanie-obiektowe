3eaadc62726b9b3f157088c560e0ad706a822772
Serhii Baraban <akakieva@gmail.com>
2024-11-08 10:16:25 +0100

    Created osoba.java

---
+++ lab4/osoba.java
@@ -0,0 +1,19 @@
+public class Osoba {
+   
+    // Konstruktor
+
+    // Gettery
+   
+    // Settery
+   
+    // Metoda wyświetlająca dane
+    
+    public static void main(String[] args) {
+        // Tworzenie obiektu Osoba
+       
+        // Użycie getterów
+        
+        // Użycie setterów
+       
+    }
+}

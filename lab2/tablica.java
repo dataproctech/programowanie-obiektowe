39e09344e2cf214e029d570dc9d84d7d73f1078a
Serhii Baraban <akakieva@gmail.com>
2024-11-08 10:14:56 +0100

    Created tablica.java

---
+++ lab2/tablica.java
@@ -0,0 +1,13 @@
+public class Tablica {
+   // Konstruktor domyślny
+    
+   // Konstruktor z parametrem
+   
+    public static void main(String[] args) {
+        // Tworzenie obiektów Tablica
+      
+        // Użycie konstruktora domyślnego
+      
+        // Użycie konstruktora z argumentem
+    }
+}

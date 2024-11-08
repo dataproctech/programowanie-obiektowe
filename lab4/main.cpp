07ea02c6916a06102611de4b9c0c184cf7a54096
Serhii Baraban <akakieva@gmail.com>
2024-11-08 10:15:59 +0100

    Created main.cpp

---
+++ lab4/main.cpp
@@ -0,0 +1,25 @@
+// #include 
+
+class Osoba {
+
+    // Konstruktor
+   
+    // Gettery
+    
+    // Settery
+   
+    // Sprawdzenie poprawności wieku
+            
+    // Metoda wyświetlająca dane
+   
+};
+
+int main() {
+    // Tworzenie obiektu Osoba
+    
+    // Użycie getterów
+    
+    // Użycie setterów
+    
+    return 0;
+}
